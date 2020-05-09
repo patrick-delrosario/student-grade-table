84792efd4839238ef52cc98551995e2ee19b8678
Patrick <pgdelrosa@gmail.com>
2020-05-09 10:20:15 -0700

    updated constructor | Patrick

--- components/apps.js
+++ components/apps.js
@@ -1,16 +1,16 @@
 class App {
+  constructor(gradeTable) {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.gradeTable = gradeTable;
+  }
+
   handleGetGradesError(error) {
     console.error("error");
   }
 
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades);
-  }
-
-  constructor (gradeTable) {
-    this.handleGetGradesError = this.handleGetGradesError.bind(this);
-    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
-    this.gradeTable = gradeTable;
   }
 
   getGrades() {
