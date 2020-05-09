8497e81fc4b2568ede5e4001333fd7db455b5110
Patrick <pgdelrosa@gmail.com>
2020-05-09 11:20:16 -0700

    updated PageHeader

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var table = document.querySelector("table");
 var gradeTable = new GradeTable(table);
 
-var app = new App(gradeTable);
+var head = document.querySelector("header");
+var average = new PageHeader(head);
+
+var app = new App(gradeTable, average);
 app.start();
