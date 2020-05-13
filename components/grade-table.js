356fa89199d61dd3e4eef9a1f8f724bd1188071a
Patrick <pgdelrosa@gmail.com>
2020-05-12 21:55:51 -0700

    updated sgt

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,6 +1,7 @@
 class GradeTable {
-  constructor (tableElement) {
+  constructor (tableElement, noGradesElement) {
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
   updateGrades (grades) {
     var tBody = this.tableElement.querySelector("tbody");
@@ -19,23 +20,28 @@ class GradeTable {
 
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
-  })
+  }
 
   renderGradeRow(data, deleteGrade) {
-    tBody.innerHTML = "";
-    for (var i = 0; i < grades.length; i++) {
-      var row = document.createElement("tr");
-      var name = document.createElement("td");
-      var course = document.createElement("td");
-      var grade = document.createElement("td");
-      var operation = document.createElement("td");
-      var btn = document.createElement("button");
-      btn.classList.add()
-      name.textContent = grades[i].name;
-      course.textContent = grades[i].course;
-      grade.textContent = grades[i].grade;
-      opperation.innerHTML = btn;
+    var dRow = document.createElement("tr")
+    var dName = document.createElement("td");
+    var dCourse = document.createElement("td");
+    var dGrade = document.createElement("td");
+    var Operation = document.createElement("td");
 
-      tBody.append(row, name, course, grade);
-    }}
+    var btn = document.createElement("button");
+    btn.classList.add("btn btn-danger");
+    btn.addEventListener("click", function () {
+      deleteGrade(data.id);
+    })
+
+    dName.textContent = grades[i].name;
+    dCourse.textContent = grades[i].course;
+    dGrade.textContent = grades[i].grade;
+    Operation.innerHTML = btn
+
+    dRow.append(dName, dCourse, dGrade, Operation);
+
+    return dRow
+  }
 }
