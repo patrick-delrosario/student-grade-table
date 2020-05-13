fa8a4f383112399ac3d13d4f0ab8210668235a23
Patrick <pgdelrosa@gmail.com>
2020-05-12 19:21:50 -0700

    enhanced App component

--- components/apps.js
+++ components/apps.js
@@ -8,6 +8,9 @@ class App {
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
+    this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
   }
 
   handleGetGradesError(error) {
@@ -63,6 +66,18 @@ class App {
         success: this.handleCreateGradeSuccess,
         error: this.handleCreateGradeError,
       })
+  }
+
+  deleteGrade(id) {
+    console.log(id);
+  }
+
+  handleDeleteGradeError(error) {
+    console.error()
+  }
+
+  handleDeleteGradeSuccess() {
+    this.getGrades(this)
   }
 
   start() {
